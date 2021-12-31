5358a63ebaefcfbbcd47ede8115c6b8b47c822e9
william <williamsilvaec@gmail.com>
2021-12-31 09:44:20 -0300

    first commit

---
+++ src/main/java/com/williamsilva/serviceproduto/ServiceProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.williamsilva.serviceproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProdutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProdutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/williamsilva/serviceproduto/ServiceProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamsilva.serviceproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProdutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
